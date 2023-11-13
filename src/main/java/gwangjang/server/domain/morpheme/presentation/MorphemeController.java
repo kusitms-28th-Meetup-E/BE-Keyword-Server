3caedb3b9179c1ef9c06808484d48fe7a370b3e5
eojin0814 <leeeojin6@gmail.com>
2023-11-13 22:27:55 +0900

    feat(#1) : Spring Batch 구현

---
+++ src/main/java/gwangjang/server/domain/morpheme/presentation/MorphemeController.java
@@ -0,0 +1,36 @@
+package gwangjang.server.domain.morpheme.presentation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import gwangjang.server.domain.morpheme.domain.service.MorphemeService;
+import gwangjang.server.domain.morpheme.domain.service.NewsAPIService;
+import io.swagger.annotations.ApiOperation;
+import kr.co.shineware.nlp.komoran.model.Token;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/keyword")
+@RequiredArgsConstructor
+public class MorphemeController {
+
+    private final NewsAPIService newsAPIService;
+    private final MorphemeService morphemeService;
+    @GetMapping("/analysis/{msg}")
+    public String analysis(@PathVariable String msg) throws JsonProcessingException {
+      String newsList1 = newsAPIService.naverAPI("주 69시간 근로시간 제도 개편");
+      String newsList2 = newsAPIService.naverAPI("이태원 참사");
+      String newsList3 = newsAPIService.naverAPI("국민연금 개혁");
+      List<Token> newsAnalysis1 =newsAPIService.analysis(newsList1);
+      List<Token> newsAnalysis2 =newsAPIService.analysis(newsList2);
+      List<Token> newsAnalysis3 =newsAPIService.analysis(newsList3);
+      morphemeService.saveOrUpdateWord(newsAnalysis1, 100 );
+      morphemeService.saveOrUpdateWord(newsAnalysis2, 200);
+      morphemeService.saveOrUpdateWord(newsAnalysis3, 300);
+      return "success";
+    }
+}
