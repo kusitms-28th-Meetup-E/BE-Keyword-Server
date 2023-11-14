a80896aa89adf29c452104bb31b7840d46300aed
eojin0814 <leeeojin6@gmail.com>
2023-11-14 09:16:17 +0900

    feat(#1) : scheduled cron

--- src/main/java/gwangjang/server/domain/morpheme/presentation/MorphemeController.java
+++ src/main/java/gwangjang/server/domain/morpheme/presentation/MorphemeController.java
@@ -6,6 +6,7 @@ import gwangjang.server.domain.morpheme.domain.service.NewsAPIService;
 import io.swagger.annotations.ApiOperation;
 import kr.co.shineware.nlp.komoran.model.Token;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,9 @@ public class MorphemeController {
 
     private final NewsAPIService newsAPIService;
     private final MorphemeService morphemeService;
-    @GetMapping("/analysis/{msg}")
-    public String analysis(@PathVariable String msg) throws JsonProcessingException {
+    //@GetMapping("/analysis/{msg}")
+    @Scheduled(cron = "* * * * * 2")
+    public String analysis() throws JsonProcessingException {
       String newsList1 = newsAPIService.naverAPI("주 69시간 근로시간 제도 개편");
       String newsList2 = newsAPIService.naverAPI("이태원 참사");
       String newsList3 = newsAPIService.naverAPI("국민연금 개혁");
