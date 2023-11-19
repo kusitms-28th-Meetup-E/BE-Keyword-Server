ab15305d6cd0a147d44716e6f29964621c8a99db
eojin0814 <leeeojin6@gmail.com>
2023-11-19 21:46:25 +0900

    feat(#14) :하단 mainpage api

---
+++ src/main/java/gwangjang/server/domain/Issue/application/dto/res/TopicIssueRes.java
@@ -0,0 +1,17 @@
+package gwangjang.server.domain.Issue.application.dto.res;
+
+import lombok.*;
+
+@Getter
+@Builder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TopicIssueRes {
+
+    private Long id;
+    private String issueTitle;
+
+    private String imgUrl;
+
+}
